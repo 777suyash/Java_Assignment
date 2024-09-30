3510d6f18004d31ce940372ad0d2a58161c40181
Suyash Bhosale <suyashbhosale1305@gmail.com>
2024-09-30 18:54:23 +0530

    Day1

---
+++ Day_1/p1.java
@@ -0,0 +1,16 @@
+class p1{
+    public static void main(String[] args) {
+        int a=0;
+        //int a=1;
+        //int a=-1;
+        if(a==0){
+            System.out.println("Number is zero");
+        }
+        else if(a>0){
+            System.out.println("Number is Positive");
+        }
+        else{
+            System.out.println("NUmber is Negative");
+        }
+    }
+}

2b5532559766645b7134b0ddde70952c89d51cff
Suyash Bhosale <suyashbhosale1305@gmail.com>
2024-10-13 14:54:41 +0530

    Day7

--- Day_7/p4.java
+++ Day_7/p4.java
@@ -1,13 +1,8 @@
 class p4{
 	public static void main(String args[]){
-		for(int i=0; i<=4; i++){
-			for(int j=0; j<=4; j++){
-				if(j>=i){
-					System.out.print("* ");
-				}
-				else{
-					System.out.print("  ");
-				}
+		for(int i=4; i>=1; i--){
+			for(int j=1; j<=i; j++){
+				System.out.print("* ");
 			}
 		System.out.println();
 		}		

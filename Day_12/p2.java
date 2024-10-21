a68a70abe4ac66c3f917755268601a8c1bbb500f
Suyash Bhosale <suyashbhosale1305@gmail.com>
2024-10-22 00:10:00 +0530

    Day 12

---
+++ Day_12/p2.java
@@ -0,0 +1,16 @@
+class p2{
+    public static void main(String args[]){
+        int a=10, b=20;
+
+        int multi = a*b;
+        int divi;
+            if(a<b){
+                divi=b/a;
+            }
+            else{
+                divi=a/b;
+            }
+        System.out.println(multi);
+        System.out.println(divi);
+    }
+}
